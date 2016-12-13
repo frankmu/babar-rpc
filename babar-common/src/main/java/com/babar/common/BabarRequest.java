027b30578cdf8b4a1a1e1ddbd7b5021932f2b163
frankmu <frankluck52@gmail.com>
2016-12-13 06:44:42 -0800

    add common module

---
+++ babar-common/src/main/java/com/babar/common/BabarRequest.java
@@ -0,0 +1,39 @@
+package com.babar.common;
+
+public class BabarRequest {
+	private String requestId;
+	private String className;
+	private String methodName;
+	private Object[] parameters;
+	private Class<?>[] parameterTypes;
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	public Object[] getParameters() {
+		return parameters;
+	}
+	public void setParameters(Object[] parameters) {
+		this.parameters = parameters;
+	}
+	public Class<?>[] getParameterTypes() {
+		return parameterTypes;
+	}
+	public void setParameterTypes(Class<?>[] parameterTypes) {
+		this.parameterTypes = parameterTypes;
+	}
+}
